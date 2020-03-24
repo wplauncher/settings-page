f1b80d20a9db38e1624087ca84dc947014ef34c4
= <=>
2020-03-24 15:36:46 -0400

    update

--- admin/class-settings-page-admin.php
+++ admin/class-settings-page-admin.php
@@ -103,7 +103,7 @@ class Settings_Page_Admin {
 	}
 	public function addPluginAdminMenu() {
 		//add_menu_page( $page_title, $menu_title, $capability, $menu_slug, $function, $icon_url, $position );
-		add_menu_page(  $this->plugin_name, 'Settings Page', 'administrator', $this->plugin_name, array( $this, 'displayPluginAdminDashboard' ), 'chart-area', 26 );
+		add_menu_page(  $this->plugin_name, 'Dashboard', 'administrator', $this->plugin_name, array( $this, 'displayPluginAdminDashboard' ), 'dashicons-chart-area', 26 );
 		
 		//add_submenu_page( '$parent_slug, $page_title, $menu_title, $capability, $menu_slug, $function );
 		add_submenu_page( $this->plugin_name, 'Settings Page Settings', 'Settings', 'administrator', $this->plugin_name.'-settings', array( $this, 'displayPluginAdminSettings' ));
